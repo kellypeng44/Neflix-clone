baf1b64efbf2c3d7d9052244a39150bf618ec593
Kelly Peng <mingyu.peng@stonybrook.edu>
2022-02-13 23:30:58 -0500

    add api models

---
+++ server/models/Movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = mongoose.Schema(
+    {
+        title: { type: String, required: true, unique: true },
+        desc: { type: String },
+        img: { type: String },
+        imgThumbnail: { type: String },
+        imgLogo: { type: String },
+        trailer: { type: String },
+        video: { type: String },            // actual movie
+        year: { type: String },
+        ageLimit: { type: Number },
+        genre: { type: String },
+        isSeries: { type: Boolean, default: false },
+    },
+    { timestamps: true }
+);
+
+module.export = mongoose.model("Movie", MovieSchema);
