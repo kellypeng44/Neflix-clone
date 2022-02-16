b38378b692bde321ee43cbce7af9e167e12ce7af
Kelly Peng <mingyu.peng@stonybrook.edu>
2022-02-16 12:28:48 -0500

    user route CRUD: delete, get, get all functions

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ const verify = require('../verifyToken');
 
 // UPDATE
 router.put("/:id", verify, async(req, res) => {
+    // check account owner and if isAdmin
     if(req.user.id === req.params.id || req.user.isAdmin){
         if(req.body.password){
             req.body.password = CryptoJS.AES.encrypt(
@@ -30,8 +31,46 @@ router.put("/:id", verify, async(req, res) => {
 });
 
 // DELETE
+router.delete("/:id", verify, async(req, res) => {
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted...");
+        } catch(err){
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You can delete only your account!");
+    }
+});
+
 // GET
+router.get("/find/:id", async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...info } = user._doc;
+        res.status(200).json(info);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
 // GET ALL
+router.get("", verify, async(req, res) => {
+    // query: ?new=true
+    const query = req.query.new;
+    if(req.user.isAdmin){
+        try {
+            const users = query ? await User.find().sort({_id: -1}).limit(10) : await User.find();
+            res.status(200).json(users);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed to see all users!");
+    }
+});
+
 // GET USER STATS
 
 module.exports = router;
