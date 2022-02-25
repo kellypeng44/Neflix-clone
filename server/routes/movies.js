5273b3cdf08d6e0f122926f23a3214b7ca5a5c3f
Kelly Peng <mingyu.peng@stonybrook.edu>
2022-02-25 10:14:56 -0500

    list crud functions

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Movie = require('../models/User'); 
+const Movie = require('../models/Movie'); 
 
 // CREATE
 router.post("/", verify, async (req, res) => {
